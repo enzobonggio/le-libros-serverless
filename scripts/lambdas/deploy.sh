d2447342afceb6a090bd481565aa5397db24276f
Enzo Bonggio <enzo.fcen@gmail.com>
2021-10-24 03:00:16 -0300

    deployed echo with lambda

--- scripts/lambdas/deploy.sh
+++ scripts/lambdas/deploy.sh
@@ -4,7 +4,7 @@ deploy() {
   local lambda_name=$(basename $1)
   echo "Deploying $lambda_name..."
   aws lambda update-function-code --function-name le_libros_$lambda_name --zip-file fileb://$1/dist/lambda.zip
-  echo "Deployed $lambda"
+  echo "Deployed $lambda_name"
 }
 
 local i=0
