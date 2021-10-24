bf1851f776ccff10e213fd70b2a501e9a7328d4c
Enzo Bonggio <enzo.fcen@gmail.com>
2021-10-24 02:35:30 -0300

    deploy script

--- scripts/lambdas/deploy.sh
+++ scripts/lambdas/deploy.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-find ./lambdas/* -maxdepth 0 -type d -exec bash -c "echo 'Deploying {}...' && cd {} && aws lambda update-function-code --function-name le_libros_{} --zip-file fileb://lambdas/{}/dist/lambda.zip" \;
+deploy() {
+  echo $1
+  local lambda_name=$(basename $1)
+  echo "Deploying $lambda_name..."
+  aws lambda update-function-code --function-name le_libros_$lambda_name --zip-file fileb://$1/dist/lambda.zip
+  echo "Deployed $lambda"
+}
+
+for lambda_path in lambdas/* ; do
+    deploy $lambda_path &
+done
+
