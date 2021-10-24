d112d5926179247a018ef6131f500d7b6deb84e6
Enzo Bonggio <enzo.fcen@gmail.com>
2021-10-24 02:52:36 -0300

    wait tasks to end

--- scripts/lambdas/deploy.sh
+++ scripts/lambdas/deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 deploy() {
-  echo $1
   local lambda_name=$(basename $1)
   echo "Deploying $lambda_name..."
   aws lambda update-function-code --function-name le_libros_$lambda_name --zip-file fileb://$1/dist/lambda.zip
   echo "Deployed $lambda"
 }
 
+local i=0
 for lambda_path in lambdas/* ; do
     deploy $lambda_path &
+    pids[$i]=$!
+    let "i++"
 done
 
+for pid in ${pids[*]}; do
+    wait $pid
+done
