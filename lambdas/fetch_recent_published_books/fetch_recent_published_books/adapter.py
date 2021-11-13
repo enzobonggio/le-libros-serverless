596321b3c5f513b03abd1083f4932f532482897e
Enzo Bonggio <enzo.fcen@gmail.com>
2021-11-13 03:14:27 -0300

    remove extra words from book code

--- lambdas/fetch_recent_published_books/fetch_recent_published_books/adapter.py
+++ lambdas/fetch_recent_published_books/fetch_recent_published_books/adapter.py
@@ -1,12 +1,16 @@
+import re
+
 from bs4 import BeautifulSoup
+from logic.href import code_from_href
 
 from fetch_recent_published_books.schemas.response import FetchBooksRecord
-from logic.href import code_from_href
 
 
 def tag_to_book(tag) -> FetchBooksRecord:
+    code = code_from_href(tag.attrs['href'])
+    code = re.search('descargar-libro-(.*)-en-.*', code).group(1)
     return FetchBooksRecord(
-        code=code_from_href(tag.attrs['href']),
+        code=code,
         title=tag.attrs['title'],
         image='https://lelibros.online' + tag.select_one('amp-img').attrs['src']
     )
