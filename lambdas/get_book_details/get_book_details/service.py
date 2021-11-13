6a95d2d1e754ade6ec1cf9bc35f795265fbbd57f
Enzo Bonggio <enzo.fcen@gmail.com>
2021-11-13 03:01:44 -0300

    book_id

--- lambdas/get_book_details/get_book_details/service.py
+++ lambdas/get_book_details/get_book_details/service.py
@@ -2,7 +2,6 @@ from get_book_details.controller import get_book_details
 
 
 def lambda_handler(event, context):
-    print(str(event))
-    query_parameters = event['queryStringParameters']
-    book = query_parameters['book']
+    path_parameters = event['pathParameters']
+    book = path_parameters['book_id']
     return get_book_details(book).to_json()
