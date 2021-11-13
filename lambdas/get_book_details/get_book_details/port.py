ce1acfa72fdea38704c62c4c5abbf53445a6b8e6
Enzo Bonggio <enzo.fcen@gmail.com>
2021-11-13 02:57:19 -0300

    comment numpy

--- lambdas/get_book_details/get_book_details/port.py
+++ lambdas/get_book_details/get_book_details/port.py
@@ -1,13 +1,13 @@
-import binascii
-from io import BytesIO
+# import binascii
+# from io import BytesIO
 
-import numpy as np
+# import numpy as np
 import requests
-import scipy.cluster
-from PIL import Image
+# import scipy.cluster
+# from PIL import Image
 from bs4 import BeautifulSoup
 
-NUM_CLUSTERS = 5
+# NUM_CLUSTERS = 5
 
 
 def get(book: str):
@@ -16,28 +16,28 @@ def get(book: str):
     return BeautifulSoup(page.content, 'html.parser')
 
 
-def colors_from_image(image):
-    print('reading image')
-    response = requests.get(image)
-    im = Image.open(BytesIO(response.content))
-    ar = np.asarray(im)
-    shape = ar.shape
-    ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
-
-    print('finding clusters')
-    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-    print('cluster centres:\n', codes)
-
-    vecs, dist = scipy.cluster.vq.vq(ar, codes)  # assign codes
-    counts, bins = np.histogram(vecs, len(codes))  # count occurrences
-
-    count_dist_list = list(zip(counts, codes))
-
-    count_dist_list.sort(key=lambda x: x[0], reverse=True)
-
-    print(count_dist_list)
-
-    hex_colors = map(lambda x: binascii.hexlify(bytearray(int(c) for c in x[1])).decode('ascii'), count_dist_list)
-    hex_colors_url = map(lambda color: "https://www.color-hex.com/color/" + color, hex_colors)
-
-    print("List of most used colors: " + str(list(hex_colors_url)))
+# def colors_from_image(image):
+#     print('reading image')
+#     response = requests.get(image)
+#     im = Image.open(BytesIO(response.content))
+#     ar = np.asarray(im)
+#     shape = ar.shape
+#     ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
+#
+#     print('finding clusters')
+#     codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+#     print('cluster centres:\n', codes)
+#
+#     vecs, dist = scipy.cluster.vq.vq(ar, codes)  # assign codes
+#     counts, bins = np.histogram(vecs, len(codes))  # count occurrences
+#
+#     count_dist_list = list(zip(counts, codes))
+#
+#     count_dist_list.sort(key=lambda x: x[0], reverse=True)
+#
+#     print(count_dist_list)
+#
+#     hex_colors = map(lambda x: binascii.hexlify(bytearray(int(c) for c in x[1])).decode('ascii'), count_dist_list)
+#     hex_colors_url = map(lambda color: "https://www.color-hex.com/color/" + color, hex_colors)
+#
+#     print("List of most used colors: " + str(list(hex_colors_url)))
