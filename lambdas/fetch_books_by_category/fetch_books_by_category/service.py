ce2c7eac53b4b980a830e4a6a54153b1edaa3ed5
Enzo Bonggio <enzo.fcen@gmail.com>
2021-11-30 23:35:37 -0300

    page number is str

--- lambdas/fetch_books_by_category/fetch_books_by_category/service.py
+++ lambdas/fetch_books_by_category/fetch_books_by_category/service.py
@@ -4,5 +4,5 @@ from fetch_books_by_category.controller import fetch_books_by_category
 def lambda_handler(event, context):
     query_parameters = event['queryStringParameters']
     category = query_parameters['category']
-    page_number = query_parameters.get('page') or 1
+    page_number = int(query_parameters.get('page') or '1')
     return fetch_books_by_category(category, page_number).to_json()
--- lambdas/fetch_recent_published_books/fetch_recent_published_books/service.py
+++ lambdas/fetch_recent_published_books/fetch_recent_published_books/service.py
@@ -3,5 +3,5 @@ from fetch_recent_published_books.controller import fetch_recent_published_books
 
 def lambda_handler(event, context):
     query_parameters = event.get('queryStringParameters') or {}
-    page_number = query_parameters.get('page') or 1
+    page_number = int(query_parameters.get('page') or '1')
     return fetch_recent_published_books(page_number).to_json()
